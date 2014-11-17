a1854c2fae5265359e9eb3c1e99a8ea649661e40
weswhitney <wesleywhitney@gmail.com>
2014-11-17 10:42:04 -0700

    finished exercise

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,5 +1,5 @@
 class Cat < ActiveRecord::Base
 
-  # add your belongs_to / has_many lines here
+  has_many :toys
 
 end
--- app/models/toy.rb
+++ app/models/toy.rb
@@ -1,5 +1,5 @@
 class Toy < ActiveRecord::Base
 
-  # add your belongs_to / has_many lines here
+  belongs_to :cat
 
 end
